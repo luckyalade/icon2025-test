51a563bb4bbea18c631feb666d82370e34daf088
Lucky Alade <luckyalade309@gmail.com>
2025-09-01 13:22:47 +0100

    Update: webamp icon right margin fix

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -115,7 +115,7 @@ const MainContent = () => {
             <img
               src="/winamp_2014.png"
               alt="webamp logo"
-              className="w-[50px] h-[50px] md:w-12 md:h-12 opacity-80 md:opacity-70 mt-1 mr-3 md:mr-0 md:mt-1 rounded-md drop-shadow-sm drop-shadow-gray-400/90"
+              className="w-[50px] h-[50px] md:w-12 md:h-12 opacity-80 md:opacity-70 mt-1 mr-5 md:mr-0 md:mt-1 rounded-md drop-shadow-sm drop-shadow-gray-400/90"
             />
             <p className="text-gray-50/90 text-xs mt-2 mr-3 md:mr-0 text-shadow-lg leading-tight w-10 ">
               {showWebamp ? "Hide Player" : "Show Player"}
